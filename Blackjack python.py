b78d5fe72ab1b29e02574dd461fe787232d07aa3
Siddharth Ghosh <50557004+GhoshSiddharth01@users.noreply.github.com>
2020-04-12 17:09:00 -0600

    Add files via upload

--- Blackjack python.py
+++ Blackjack python.py
@@ -16,7 +16,8 @@ player_cards = []
 print()
 print("Note: read only bottom line to know outcome of the game")
 print()
-
+print("Type restart at anytime to restart the game")
+print()
 #Deal the cards
 while len(dealer_cards) != 2:
         dealer_cards.append(random.randint(1,11))
@@ -39,34 +40,43 @@ while sum(player_cards) < 21:
     action_taken = str(input("Do you want to stay or hit? "))
     if action_taken == "hit":
         player_cards.append(random.randint(1, 11))
-        print("You now have a total of " + str(sum(player_cards)) + "from these cards ", player_cards)
+        print("You now have a total of " + str(sum(player_cards)) + " " + "from these cards ", player_cards)
+    if sum(dealer_cards) > 17 and action_taken == "hit":
+        print("The dealer has stayed")
+    if sum(dealer_cards) < 17 and action_taken == "stay":
+        dealer_cards.append(random.randint(1, 11))
+    if sum(dealer_cards) < 17 and action_taken == "hit":
+        dealer_cards.append(random.randint(1,11))
     if action_taken == "stay":
         print("You have a total of " + str(sum(player_cards)) + " with ", player_cards)
+        print("The dealer has a total of " + str(sum(dealer_cards)) + "with", dealer_cards)
+        if 22 > sum(player_cards) > sum(dealer_cards):
+            print("Player 1 wins the game!")
+            break
         if 22 > sum(dealer_cards) > sum(player_cards):
-            print("Dealer wins the game")
+            print("Dealer wins the game:")
             break
-        else:
-            print("Player 1 wins!")
-            break
+    if sum(player_cards) > 21:
+            print("Player 1 busts, dealer wins")
+    if action_taken == "restart":
+                break
 
 #Dealer janky AI
-if sum(dealer_cards) < 16 and action_taken == "stay":
-    dealer_cards.append(random.randint(1, 11))
     
-if sum(dealer_cards) > 16 and action_taken == "hit":
-    print("The dealer has a total of " + str(sum(dealer_cards)) + "with", dealer_cards)
+
 
 if sum(dealer_cards) > 21:
     print("Dealer busts, Player 1 wins the game!")
-
-if sum(player_cards) > 21:
-    print("Player 1 busts, Dealer wins")
+    
+    
 elif sum(player_cards) == 21:
     print("Player 1 has blackjack!")
     print("Player 1 wins!")
+    
 
 if sum(player_cards) == sum(dealer_cards):
     print("It's a tie")
 
 if sum(player_cards) > 21 and sum(dealer_cards) > 21:
     print("Both Dealer and Player 1 bust, it's a tie")
+    
